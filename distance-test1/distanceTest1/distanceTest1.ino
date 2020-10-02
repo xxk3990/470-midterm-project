b0cf6093df8a4d707e781ec65725bf71175db514
Xander Kaylan <xxk3990@rit.edu>
2020-10-02 16:12:06 -0400

    added led2

--- distance-test1/distanceTest1/distanceTest1.ino
+++ distance-test1/distanceTest1/distanceTest1.ino
@@ -5,11 +5,13 @@
 //The model of the sensor is "GP2Y0A21YK0F"
 //The sensor output pin is attached to the pin A0
 SharpIR sensor(SharpIR::GP2Y0A21YK0F, A0);
-int led1 = 13;
+int led1 = 13; // red led
+int led2 = 12; //green led
 
 void setup() {
   Serial.begin(9600); //Enable the serial comunication
   pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
 }
 
 void loop() {
@@ -19,9 +21,11 @@ void loop() {
   Serial.print("Distance: ");
   Serial.print(distance);
   if(distance > 20) {
-    digitalWrite(led1, HIGH);
-  } else {
+     digitalWrite(led2, HIGH);
     digitalWrite(led1, LOW);
+  } else {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
   }
   Serial.println("cm");
   delay(500);
